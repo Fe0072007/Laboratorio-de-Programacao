df918f85d7ee959d023b8beb2139a14c0371cd9a
Fe0072007 <ruannandes4567@gmail.com>
2026-09-14 09:11:21 -0300

    Segunda questão

---
+++ AtividadeProva/Q2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define N = 10;
+
+int main(){
+
+    int x1,x2,x3;
+
+    for(int i = 0;i <= 10; i++){
+        printf("Insira a amostra %d:\n", i+1);
+        x3 = x2;
+        x2 = x1;
+        scanf("%d", &x1);
+
+        if(i >= 2)
+            printf("y[%d]:%.2lf\n", i-1, (double) (x1 + x2 + x3)/3);
+    }
+
+    return 0;
+
+}
