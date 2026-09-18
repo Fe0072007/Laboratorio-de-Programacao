821d444e62e29b46e5129b81944588c86c1994a5
Fe0072007 <ruannandes4567@gmail.com>
2026-09-18 11:41:31 -0300

    algoritmos feitos em sala

--- Desafios/armarios.c
+++ Desafios/armarios.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+void printBin(unsigned char bin){
+    for (int i = 0; i <=7; i++){
+        printf("%d", (bin>>i)&1);
+    }
+    printf("\n");
+}
 
 int main(){
     
@@ -8,6 +16,7 @@ int main(){
     unsigned char op;
 
     while(ctrl){
+        srand(time(0));
         unsigned char lib = 0x01;
         unsigned short int pos = rand() % 8;
         puts("1 - Ocupar armário.");
@@ -20,16 +29,16 @@ int main(){
             case 1:
                 lib = lib << pos;
                 armarios += lib;
-                printf("%hhu\n", armarios);
+                printBin(armarios);
                 break;
 
             case 2:
-                puts("Insira a posição (0 - 7): ");
+                puts("Insira a posição (1 - 8): ");
                 scanf("%hd", &pos);
 
                 lib = lib << (pos - 1);
                 armarios = armarios ^ lib;
-                printf("%hhu\n", armarios);
+                printBin(armarios);
                 break;
             case 3:
                 ctrl = 0;
